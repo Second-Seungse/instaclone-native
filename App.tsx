0fc65c9866a45e30541f463e88dad9eeffb21bb9
SeungseOh <ohsse12521@gmail.com>
2021-06-06 20:19:05 +0900

    #14.0 Your First Navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import { StyleSheet, Text, View } from "react-native";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -34,18 +34,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Goodxx!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
