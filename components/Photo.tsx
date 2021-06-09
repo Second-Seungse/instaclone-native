aab7a1eaa036c774d92dcb9e9d3eca5e93823a34
SeungseOh <ohsse12521@gmail.com>
2021-06-09 23:20:33 +0900

    #15.6 Photo part One

---
+++ components/Photo.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components/native";
+import { useWindowDimensions } from "react-native";
+
+const Container = styled.View``;
+const Header = styled.View``;
+const UserAvatar = styled.Image<any>``;
+const Username = styled.Text`
+  color: white;
+`;
+const File = styled.Image``;
+const Actions = styled.View``;
+const Action = styled.TouchableOpacity``;
+const Caption = styled.View``;
+const CaptionText = styled.Text`
+  color: white;
+`;
+const Likes = styled.Text`
+  color: white;
+`;
+
+function Photo({ id, user, caption, file, isLiked, likes }) {
+  // * 현재 화면의 가로,세로 를 구한다
+  const { width, height } = useWindowDimensions();
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <Username>{user.username}</Username>
+      </Header>
+      <File
+        style={{
+          width,
+          height: height - 500,
+        }}
+        source={{ uri: file }}
+      />
+      <Actions>
+        <Action />
+        <Action />
+      </Actions>
+      <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+      <Caption>
+        <Username>{user.username}</Username>
+        <CaptionText>{caption}</CaptionText>
+      </Caption>
+    </Container>
+  );
+}
+
+Photo.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
+  caption: PropTypes.string,
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  commentNumber: PropTypes.number.isRequired,
+};
+export default Photo;
