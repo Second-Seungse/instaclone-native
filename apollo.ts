941fd7bb764827a010d68ff21a7a071f9eed236e
SeungseOh <ohsse12521@gmail.com>
2021-06-09 19:30:43 +0900

    #15.4 Apollo Auth

--- apollo.ts
+++ apollo.ts
@@ -1,20 +1,44 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
 export const tokenVar = makeVar("");
 
+const TOKEN: string = "token";
+
 export const logUserIn = async (token) => {
-  await AsyncStorage.multiSet([
-    ["token", token],
-    ["loggedIn", "yes"],
-  ]);
+  await AsyncStorage.setItem(TOKEN, token);
   isLoggedInVar(true);
   tokenVar(token);
 };
 
+export const logUserOut = async () => {
+  await AsyncStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  tokenVar(null);
+};
+
+const httpLink = createHttpLink({
+  uri: "http://tender-kangaroo-97.loca.lt/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: "https://swift-bullfrog-33.loca.lt/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 export default client;
