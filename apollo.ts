4499ebddc07116675917ff1e1d87f39d101ecc47
SeungseOh <ohsse12521@gmail.com>
2021-06-10 22:36:03 +0900

    #15.10 Infinite Scrolling part One

--- apollo.ts
+++ apollo.ts
@@ -26,7 +26,7 @@ export const logUserOut = async () => {
 };
 
 const httpLink = createHttpLink({
-  uri: "https://dangerous-husky-81.loca.lt/graphql",
+  uri: "https://strange-bear-82.loca.lt/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
