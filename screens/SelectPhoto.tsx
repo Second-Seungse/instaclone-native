5c8528c15f6cb838554343f037d92e7bf975d2a5
SeungseOh <ohsse12521@gmail.com>
2021-06-12 23:30:12 +0900

    #17.5 Select Photo part Four

--- screens/SelectPhoto.tsx
+++ screens/SelectPhoto.tsx
@@ -8,38 +8,44 @@ import {
   TouchableOpacity,
   useWindowDimensions,
 } from "react-native";
+import { colors } from "../colors";
 
 const Container = styled.View`
   flex: 1;
   background-color: black;
 `;
-
 const Top = styled.View`
   flex: 1;
   background-color: black;
 `;
-
 const Bottom = styled.View`
   flex: 1;
   background-color: black;
 `;
-
 const ImageContainer = styled.TouchableOpacity``;
 const IconContainer = styled.View`
   position: absolute;
   bottom: 5px;
   right: 0px;
 `;
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
 
-export default function SelectPhoto() {
+export default function SelectPhoto({ navigation }) {
   const [ok, setOk] = useState(false);
   const [photos, setPhotos] = useState([]);
   const [chosenPhoto, setChosenPhoto] = useState("");
+
   const getPhotos = async () => {
     const { assets: photos } = await MediaLibrary.getAssetsAsync();
     setPhotos(photos);
     setChosenPhoto(photos[0]?.uri);
   };
+
   const getPermissions = async () => {
     const { granted, canAskAgain } = await MediaLibrary.getPermissionsAsync();
     if (granted === false && canAskAgain) {
@@ -53,9 +59,23 @@ export default function SelectPhoto() {
       getPhotos();
     }
   };
+
+  const HeaderRight = () => (
+    <TouchableOpacity>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+
   useEffect(() => {
     getPermissions();
   }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRight,
+    });
+  }, []);
+
   const numColumns = 4;
   const { width } = useWindowDimensions();
   const choosePhoto = (uri) => {
@@ -68,7 +88,11 @@ export default function SelectPhoto() {
         style={{ width: width / numColumns, height: 100 }}
       />
       <IconContainer>
-        <Ionicons name="checkmark-circle" size={18} color="white" />
+        <Ionicons
+          name="checkmark-circle"
+          size={18}
+          color={photo.uri === chosenPhoto ? colors.blue : "white"}
+        />
       </IconContainer>
     </ImageContainer>
   );
