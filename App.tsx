1e587f7aa4bbe6d94058792c2e90539742518f4f
SeungseOh <ohsse12521@gmail.com>
2021-06-16 07:39:42 +0900

    #18.8 Room Screen part Six

--- App.tsx
+++ App.tsx
@@ -34,11 +34,11 @@ export default function App() {
       isLoggedInVar(true);
       tokenVar(token);
     }
-    await persistCache({
+    /* await persistCache({
       cache,
       storage: new AsyncStorageWrapper(AsyncStorage),
       serialize: false,
-    });
+    }); */
     return preloadAssets();
   };
 
